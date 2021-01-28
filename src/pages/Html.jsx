2a7de994233d33412e47b572613ff4a50e7650d7
Danil Agafonov <danil.agafonov@gmail.com>
2021-01-28 02:04:15 +0100

    Migrate to Webpack

---
+++ src/pages/Html.jsx
@@ -0,0 +1,37 @@
+const Html = () => (
+  <html lang="en">
+    <head>
+      <meta charSet="utf-8" />
+      <title>Flatstack Warsaw</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#fafafa" />
+      <link rel="apple-touch-icon" sizes="138x138" href="/logo138.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+      <meta
+        name="description"
+        content="We are Flatstack and we are hiring software engineers for our Warsaw office. Join us!"
+      />
+      <meta
+        name="keywords"
+        content="flatstack, software engineer, ruby on rails, warsaw, hiring"
+      />
+
+      <meta property="og:title" content="Flatstack Warsaw. Join us!" />
+      <meta
+        property="og:description"
+        content="We are Flatstack and we are hiring software engineers for our Warsaw office. Join us!"
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://www.flatstack.com/warsaw" />
+      <meta
+        property="og:image"
+        content="https://www.flatstack.com/warsaw/logo500.png"
+      />
+    </head>
+    <body>
+      <p>Hello Flatstack!</p>
+    </body>
+  </html>
+);
+
+export default Html;
