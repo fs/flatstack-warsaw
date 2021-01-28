9195d187de5a106cad99756d056a12e3d98232b1
Danil Agafonov <danil.agafonov@flatstack.com>
2021-01-28 02:57:13 +0100

    Add L10n

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,12 @@
 import { renderToStaticMarkup } from 'react-dom/server';
-import Html from './Html';
+import Content from './shared/Content';
+import L10nContext from './shared/L10nContext';
+import enLocale from '../locales/en';
 
-const EnPage = () => <Html />;
+const EnPage = () => (
+  <L10nContext.Provider locale="en" values={enLocale}>
+    <Content />
+  </L10nContext.Provider>
+);
 
 export default renderToStaticMarkup(<EnPage />);
---
+++ src/pages/shared/L10nContext.jsx
@@ -0,0 +1,47 @@
+import React, { useCallback, useContext } from 'react';
+import enLocale from '../../locales/en';
+
+const Context = React.createContext(null);
+
+const noTranslationFn = (key) => () => {
+  throw new Error(`translation for "${key}" not found`);
+};
+
+export const Provider = ({ locale, values, children }) => {
+  const t = useCallback(
+    (key, options) => {
+      const translation =
+        values.translations[key] ||
+        enLocale.translations[key] ||
+        noTranslationFn(key);
+
+      if (typeof translation === 'function') {
+        return translation(options);
+      }
+
+      return translation;
+    },
+    [values],
+  );
+
+  return (
+    <Context.Provider value={{ t, values, locale }}>
+      {children}
+    </Context.Provider>
+  );
+};
+
+export const useL10n = () => {
+  const contextValue = useContext(Context);
+
+  if (contextValue === null) {
+    throw new Error('useL10n cannot be used without L10nContext.Provider');
+  }
+
+  return contextValue;
+};
+
+export default {
+  Provider,
+  useL10n,
+};
