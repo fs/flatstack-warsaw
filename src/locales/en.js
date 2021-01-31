5056bcf3e0ee725cb4b5df3b97c6ce22d97cddc6
Danil Agafonov <danil.agafonov@gmail.com>
2021-01-31 22:38:29 +0100

    Reformat

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,10 +1,13 @@
 export default {
   translations: {
     title: 'Flatstack Warsaw',
-    metaDescription: 'We are Flatstack and we are hiring software engineers for our Warsaw office. Join us!',
-    metaKeywords: 'flatstack, software engineer, ruby on rails, warsaw, hiring, relocate',
+    metaDescription:
+      'We are Flatstack and we are hiring software engineers for our Warsaw office. Join us!',
+    metaKeywords:
+      'flatstack, software engineer, ruby on rails, warsaw, hiring, relocate',
     'og:title': 'Flatstack Warsaw. Join us!',
-    'og:description': 'We are Flatstack and we are hiring software engineers for our Warsaw office. Join us!',
-    welcome: 'Hello! We are Flatstack!'
-  }
-}
+    'og:description':
+      'We are Flatstack and we are hiring software engineers for our Warsaw office. Join us!',
+    welcome: 'Hello! We are Flatstack!',
+  },
+};
--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,10 +1,13 @@
 export default {
   translations: {
     title: 'Flatstack Варшава',
-    metaDescription: 'Мы — Flatstack и мы нанимает инженеров для нашего офиса в Варшаве. Присоединяйся к нам!',
-    metaKeywords: 'flatstack, software engineer, ruby on rails, warsaw, hiring, вакансия, релокейт',
+    metaDescription:
+      'Мы — Flatstack и мы нанимает инженеров для нашего офиса в Варшаве. Присоединяйся к нам!',
+    metaKeywords:
+      'flatstack, software engineer, ruby on rails, warsaw, hiring, вакансия, релокейт',
     'og:title': 'Flatstack Варшава. Присоединяйся к нам!',
-    'og:description': 'Мы — Flatstack и мы нанимает инженеров для нашего офиса в Варшаве. Присоединяйся к нам!',
-    welcome: 'Привет! Мы — Flatstack!'
-  }
-}
+    'og:description':
+      'Мы — Flatstack и мы нанимает инженеров для нашего офиса в Варшаве. Присоединяйся к нам!',
+    welcome: 'Привет! Мы — Flatstack!',
+  },
+};
