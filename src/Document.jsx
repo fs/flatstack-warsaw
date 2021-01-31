b9c5a82f089fcb583b15923fc3e443a006a1fad6
Danil Agafonov <danil.agafonov@gmail.com>
2021-01-31 23:10:03 +0100

    Fix Helmet

--- src/Document.jsx
+++ src/Document.jsx
@@ -1,11 +1,16 @@
-const Document = ({ children, styles, helmet }) => {
-  return (
-    <html>
-      <head>{styles}</head>
-      {JSON.stringify(helmet)}
-      <body dangerouslySetInnerHTML={{ __html: children }} />
-    </html>
-  );
-};
+const Document = ({ children, styles, helmet }) => (
+  <html {...helmet.htmlAttributes.toComponent()}>
+    <head>
+      {helmet.title.toComponent()}
+      {helmet.meta.toComponent()}
+      {helmet.link.toComponent()}
+      {styles}
+    </head>
+    <body
+      dangerouslySetInnerHTML={{ __html: children }}
+      {...helmet.bodyAttributes.toComponent()}
+    />
+  </html>
+);
 
 export default Document;
