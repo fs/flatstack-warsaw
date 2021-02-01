068ca23cda2764731a7d802112c11276daf59693
Danil Agafonov <danil.agafonov@flatstack.com>
2021-02-01 02:28:53 +0100

    Fix Lighthouse offenses

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,21 @@
 import styled from 'styled-components';
 import Container from './Container';
+import { useL10n } from './L10nContext';
 
 const Wrapper = styled.header``;
 
 const Logo = styled.img``;
 
-const Header = () => (
-  <Wrapper>
-    <Container>
-      <Logo src="/logo.svg" />
-    </Container>
-  </Wrapper>
-);
+const Header = () => {
+  const { t } = useL10n();
+
+  return (
+    <Wrapper>
+      <Container>
+        <Logo src="/logo.svg" alt={t('logoAlt')} width="139" height="138" />
+      </Container>
+    </Wrapper>
+  );
+};
 
 export default Header;
