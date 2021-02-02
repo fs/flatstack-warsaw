0b24ca9532346cf5af77a43b8ef34a9a3e06307a
Danil Agafonov <danil.agafonov@gmail.com>
2021-02-02 23:17:53 +0100

    Improve `t` helper

--- src/locales/en.js
+++ src/locales/en.js
@@ -10,12 +10,14 @@ export default {
       'We are Flatstack and we are hiring software engineers for our Warsaw office. Join us!',
     logoAlt: 'Flatstack logo',
     welcome: 'Hello! We are Flatstack!',
-    aboutTeam: 'About team',
-    aboutTeamText: (
-      <>
-        <p>We are blah blah blah.</p>
-        <p>Example of splitting text into paragraphs in translations.</p>
-      </>
-    ),
+    aboutTeam: {
+      title: 'About team',
+      text: (
+        <>
+          <p>We are blah blah blah.</p>
+          <p>Example of splitting text into paragraphs in translations.</p>
+        </>
+      )
+    },
   },
 };
