4b81cfe81d4bc587b28a57b401a91aa56642b202
Danil Agafonov <danil.agafonov@gmail.com>
2021-10-11 19:33:40 +0200

    Get rid of breakpoints

--- src/components/atoms/Container.jsx
+++ src/components/atoms/Container.jsx
@@ -1,33 +1,11 @@
-import styled, { css } from 'styled-components';
-
-const getMaxWidth = (theme, index) => {
-  const maxWidth = theme.containerMaxWidths[index];
-
-  if (maxWidth >= 0 && maxWidth <= 1) {
-    return `${maxWidth * 100}%`;
-  }
-
-  return `${maxWidth}em`;
-};
-
-const generateMedia = (theme) =>
-  theme.breakpoints.map(
-    (breakpoint, index) => css`
-      @media (min-width: ${breakpoint}em) {
-        max-width: ${getMaxWidth(theme, index)};
-        padding-right: 1em;
-        padding-left: 1em;
-      }
-    `,
-  );
+import styled from 'styled-components';
 
 const Container = styled.div`
-  padding-right: 0.5em;
-  padding-left: 0.5em;
+  padding-right: 1em;
+  padding-left: 1em;
+  max-width: 75em;
   width: 100%;
   margin: 0 auto;
-
-  ${({ theme }) => generateMedia(theme)};
 `;
 
 export default Container;
--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,4 @@
 export default {
-  breakpoints: [36, 48, 62, 75],
-  containerMaxWidths: [1, 1, 1, 75],
   colors: {
     accent: '#fa0e1c',
     primary: '#000000',
