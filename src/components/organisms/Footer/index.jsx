09657dd276677c05f66960740238ae63c4b7b2c3
Danil Agafonov <danil.agafonov@flatstack.com>
2021-10-19 22:23:04 +0200

    Fix color-contrast issue

--- src/components/organisms/Footer/index.jsx
+++ src/components/organisms/Footer/index.jsx
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
+import { lighten } from 'polished';
 import Link from '../../atoms/Link';
 import Container from '../../atoms/Container';
 
 const Wrapper = styled.footer`
   padding: 1em 0;
-  background-color: ${({ theme }) => theme.colors.primary};
-  color: ${({ theme }) => theme.colors.secondary};
+  color: ${({ theme }) => lighten(0.2, theme.colors.text)};
 
   ${Link} {
     color: ${({ theme }) => theme.colors.secondary};
