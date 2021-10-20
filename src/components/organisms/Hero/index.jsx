9883c2e5cb64945d2858564c4cf31f9674406251
Danil Agafonov <danil.agafonov@gmail.com>
2021-10-20 20:54:40 +0200

    Add responsiveness for Hero section

--- src/components/organisms/Hero/index.jsx
+++ src/components/organisms/Hero/index.jsx
@@ -4,41 +4,50 @@ import ButtonLink, { variants as buttonVariants } from '../../atoms/ButtonLink';
 import { useL10n } from '../../L10nContext';
 import titleBgPath from './title-bg.jpeg';
 
+const StyledSection = styled(Section)`
+  margin: 5em 0;
+`;
+
 const Title = styled.h1`
-  font-size: 6em;
+  // depend on viewport width, but not bigger than 5em and not smaller than 3em
+  font-size: max(min(5em, 15vw), 3em);
   font-weight: 900;
   background: url('${titleBgPath}') no-repeat;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
-  margin: 5rem 0 2rem;
+  margin: 0 0 0.3em;
+  line-height: 1;
 `;
 
-const Description = styled.span`
-  font-size: 2rem;
+const Subtitle = styled.span`
+  // depend on viewport width, but not bigger than 2em and not smaller than 1.25em
+  font-size: max(min(2em, 6vw), 1.25em);
   margin: 2rem 0;
 `;
 
-const DescriptionHighlight = styled.span`
+const SubtitleHighlight = styled.span`
   color: ${({ theme }) => theme.colors.accent};
 `;
 
 const TechnologiesWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
-  margin: 2rem -0.5rem;
+  gap: 0.5em;
+  margin: 2em 0 3em;
+  max-width: 35em;
 `;
 
 const Technology = styled.div`
-  font-size: 0.5em;
+  font-size: 1em;
   padding: 0.5em 1em;
   border-radius: 0.7em;
-  margin: 0.5em 0.5em;
   background-color: ${({ theme }) => theme.colors.primary};
 `;
 
 const ActionsWrapper = styled.div`
   display: flex;
   gap: 1em;
+  flex-wrap: wrap;
 `;
 
 const technologies = [
@@ -58,30 +67,26 @@ const Hero = () => {
   const { t } = useL10n();
 
   return (
-    <Section>
+    <StyledSection>
       <Title>Fullstack Developer</Title>
-      <Description>
-        {t('heroDescription.where')}
-        <DescriptionHighlight>
-          {' '}
-          &#123;{t('heroDescription.condition')}&#125;
-        </DescriptionHighlight>
-        <TechnologiesWrapper>
-          {technologies.map((technology) => (
-            <Technology>{technology}</Technology>
-          ))}
-        </TechnologiesWrapper>
-      </Description>
+      <Subtitle>
+        {t('hero.subtitle', { Highlight: SubtitleHighlight })}
+      </Subtitle>
+      <TechnologiesWrapper>
+        {technologies.map((technology) => (
+          <Technology>{technology}</Technology>
+        ))}
+      </TechnologiesWrapper>
       <ActionsWrapper>
-        <ButtonLink href="#">{t('heroActions.apply')}</ButtonLink>
-        <ButtonLink href="#" variant={buttonVariants.TEXT}>
-          {t('heroActions.recommend')}
+        <ButtonLink href="#">{t('hero.actions.apply')}</ButtonLink>
+        <ButtonLink href="#" variant={buttonVariants.BORDERED}>
+          {t('hero.actions.recommend')}
         </ButtonLink>
-        <ButtonLink href="#" variant={buttonVariants.TEXT}>
-          {t('heroActions.share')}
+        <ButtonLink href="#" variant={buttonVariants.BORDERED}>
+          {t('hero.actions.share')}
         </ButtonLink>
       </ActionsWrapper>
-    </Section>
+    </StyledSection>
   );
 };
 
