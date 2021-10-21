8c4133a78865e2c6ea3edcde721bbb4b3ddb3dc1
Danil Agafonov <danil.agafonov@flatstack.com>
2021-10-21 03:17:02 +0200

    Hero: Share button - native share

--- src/components/organisms/Hero/index.jsx
+++ src/components/organisms/Hero/index.jsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 import Section from '../../molecules/Section';
-import ButtonLink, { variants as buttonVariants } from '../../atoms/ButtonLink';
+import ButtonLink, {
+  variants as buttonLinkVariants,
+} from '../../atoms/ButtonLink';
+import Button, { variants as buttonVariants } from '../../atoms/Button';
 import { useL10n } from '../../L10nContext';
 import titleBgJpgPath from './title-bg.jpg?inline';
 import titleBgWebpPath from './title-bg.webp?inline';
@@ -85,6 +88,27 @@ const technologies = [
 const Hero = () => {
   const { t } = useL10n();
 
+  const handleShare = async () => {
+    const shareData = {
+      title: t('og:title'),
+      text: t('og:description'),
+      url: '/',
+    };
+
+    if (typeof navigator.share !== 'undefined') {
+      try {
+        await navigator.share(shareData);
+        return;
+      } catch (err) {
+        if (err.name === 'AbortError') {
+          return;
+        }
+      }
+    }
+
+    console.log('alternative share');
+  };
+
   return (
     <StyledSection>
       <InnerWrapper>
@@ -99,12 +123,12 @@ const Hero = () => {
         </TechnologiesWrapper>
         <ActionsWrapper>
           <ButtonLink href="#">{t('hero.actions.apply')}</ButtonLink>
-          <ButtonLink href="#" variant={buttonVariants.BORDERED}>
+          <ButtonLink href="#" variant={buttonLinkVariants.BORDERED}>
             {t('hero.actions.recommend')}
           </ButtonLink>
-          <ButtonLink href="#" variant={buttonVariants.BORDERED}>
+          <Button variant={buttonVariants.BORDERED} onClick={handleShare}>
             {t('hero.actions.share')}
-          </ButtonLink>
+          </Button>
         </ActionsWrapper>
       </InnerWrapper>
     </StyledSection>
