5a6c6d1f4a47d9894ed566fa1e8de130b72952a3
Danil Agafonov <danil.agafonov@flatstack.com>
2021-10-24 19:11:46 +0200

    Handle error for Share button modal

--- src/components/organisms/Hero/ShareButton.jsx
+++ src/components/organisms/Hero/ShareButton.jsx
@@ -1,4 +1,5 @@
 import { useState, lazy, Suspense } from 'react';
+import { useErrorBoundary } from 'preact/hooks';
 import styled from 'styled-components';
 import { useL10n } from '../../L10nContext';
 import Button, { variants as buttonVariants } from '../../atoms/Button';
@@ -18,6 +19,9 @@ const StyledLoadingIcon = styled(LoadingIcon)`
 const ShareButton = ({ children }) => {
   const { t } = useL10n();
   const [isModalOpen, setIsModalOpen] = useState(false);
+  useErrorBoundary(() => {
+    window.location.reload();
+  });
 
   const handleShare = async () => {
     const shareData = {
