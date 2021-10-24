b355f6e0f7939e9359c041160c51444be5613982
Danil Agafonov <danil.agafonov@flatstack.com>
2021-10-24 03:21:44 +0200

    More Lazy hydration

--- src/components/organisms/Feedbacks/index.jsx
+++ src/components/organisms/Feedbacks/index.jsx
@@ -6,7 +6,7 @@ export const ID = 'feedbacks-section';
 const Feedbacks = () => {
   const { t } = useL10n();
   return (
-    <Section id={ID}>
+    <Section id={ID} dangerouslySetInnerHTML={{ __html: '' }}>
       <Section.Title>{t('feedbacks.title')}</Section.Title>
     </Section>
   );
