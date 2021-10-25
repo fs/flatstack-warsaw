4d11e61ecb260be7461ed4b18b9cfaa68d75f95b
Danil Agafonov <danil.agafonov@flatstack.com>
2021-10-25 18:12:13 +0200

    Refactoring

--- src/components/atoms/Card.jsx
+++ src/components/atoms/Card.jsx
@@ -1,9 +1,27 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+export const types = {
+  BORDERED: 'BORDERED',
+  FILL: 'FILL',
+};
+
+const cssByType = {
+  [types.BORDERED]: css`
+    border: 1px solid ${({ theme }) => theme.colors.secondary};
+  `,
+  [types.FILL]: css`
+    background-color: ${({ theme }) => theme.colors.primary};
+  `,
+};
 
 const Card = styled.div`
   padding: 2em;
-  border: 1px solid ${({ theme }) => theme.colors.secondary};
   border-radius: 1em;
+  ${({ type }) => cssByType[type]}
 `;
+
+Card.defaultProps = {
+  type: types.BORDERED,
+};
 
 export default Card;
