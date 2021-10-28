15d8d66207a4bc452909f8de03222b2380fdeb9f
Danil Agafonov <danil.agafonov@gmail.com>
2021-10-29 01:31:57 +0200

    Reorganize pages structure and use Helmet only in build time

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,24 @@
 import 'regenerator-runtime/runtime';
 
 import ReactDOM from 'react-dom';
-import { HelmetProvider } from 'react-helmet-async';
 import { ThemeProvider } from 'styled-components';
-import EnPage from './components/pages/index';
-import RuPage from './components/pages/ru';
+import L10nContext from './components/L10nContext';
+import Page from './components/Page';
+import enLocale from './locales/en';
+import ruLocale from './locales/ru';
 import theme from './theme';
 
 const target = document.getElementById('root');
 
 const App = () => (
-  <HelmetProvider>
-    <ThemeProvider theme={theme}>
-      {window.location.pathname === '/ru' ? <RuPage /> : <EnPage />}
-    </ThemeProvider>
-  </HelmetProvider>
+  <ThemeProvider theme={theme}>
+    <L10nContext.Provider
+      locale={window.location.pathname === '/ru' ? 'ru' : 'en'}
+      values={window.location.pathname === '/ru' ? ruLocale : enLocale}
+    >
+      <Page />
+    </L10nContext.Provider>
+  </ThemeProvider>
 );
 
 ReactDOM.hydrate(<App />, target);
--- src/webpack-templates/index.js
+++ src/webpack-templates/index.js
@@ -1,4 +1,10 @@
-import EnPage from '../components/pages/index';
+import L10nContext from '../components/L10nContext';
+import Page from '../components/Page';
+import enLocale from '../locales/en';
 import render from '../render';
 
-export default render(EnPage);
+export default render(() => (
+  <L10nContext.Provider locale="en" values={enLocale}>
+    <Page />
+  </L10nContext.Provider>
+));
