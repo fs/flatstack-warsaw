2edd3013ec2e6f6d805974588c1dd4649fda2072
Danil Agafonov <danil.agafonov@flatstack.com>
2021-10-28 17:13:36 +0200

    Remove unused meta

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -41,7 +41,6 @@ const Page = () => {
         <meta charSet="utf-8" />
         <title>{t('title')}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#fafafa" />
         <link
           rel="icon"
           type="image/x-icon"
