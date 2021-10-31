164fab08891539595168c0f557f72bc184128a73
Danil Agafonov <danil.agafonov@flatstack.com>
2021-10-31 18:44:25 +0100

    Fix ESLint offense

--- src/components/organisms/Warsaw/index.jsx
+++ src/components/organisms/Warsaw/index.jsx
@@ -35,7 +35,7 @@ const Warsaw = () => {
           <div dangerouslySetInnerHTML={{ __html: innerHtmlRef.current }} />
         }
       >
-        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
+        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
         <div
           dangerouslySetInnerHTML={
             shouldBeHydrated ? undefined : { __html: '' }
